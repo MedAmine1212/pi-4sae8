f6afd55eed695d1655020f94d25ff5ce79c11ba9
MedAmine1212 <khaili.amine@hotmail.fr>
2022-02-17 20:35:08 +0100

    1st comment

---
+++ src/main/java/com/pi/dev/models/Post.java
@@ -0,0 +1,31 @@
+package com.pi.dev.models;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Post implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long postId;
+
+}
---
+++ src/main/java/com/pi/dev/serviceInterface/IPostService.java
@@ -0,0 +1,11 @@
+package com.pi.dev.serviceInterface;
+
+import java.util.List;
+
+import com.pi.dev.models.Post;
+
+public interface IPostService {
+
+	List<Post> findAll();
+
+}
