95b55edbdfb2bb85536b67f01613fc4a1a0c552e
Sk4nd3r <skanderkahouli26@hotmail.com>
2022-02-18 16:59:07 +0100

    test

---
+++ PiDev/src/test/java/com/pi/dev/PiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
